7aa4e7ff426203071c04e85be58f650ebd58be49
VPACHCHU <vpachchu27@gmail.com>
2023-01-03 21:03:00 +0530

    about commit

--- my-portfolio-with-react/src/pages/AboutPage.jsx
+++ my-portfolio-with-react/src/pages/AboutPage.jsx
@@ -2,9 +2,9 @@ function AboutPage()
 {
     return(
         <div className="h1container">
-            <h1>Welcome to About page!!</h1>
+            <h1>Welcome to About page!</h1>
         </div>
     )
-
+    
 }
 export default AboutPage;
